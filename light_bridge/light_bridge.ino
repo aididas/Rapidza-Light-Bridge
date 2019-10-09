9c2362f8d4b9152b1167035d580eebd37e4b8af6
Mihail Bondarenco <441044@student.fontys.nl>
2019-10-09 13:06:52 +0200

    fix(*) continious adding when shaded

--- light_bridge/light_bridge.ino
+++ light_bridge/light_bridge.ino
@@ -1,30 +1,37 @@
 #include "Display.h"
 const int LDR = A2;
 const int GreenLED = 5;
-int Dark_Threshold = 220;
-int Light_Threshold = 500;
+
+enum State {
+  LIGHTED, SHADED
+};
+
+State state = LIGHTED;
+
+int Dark_Threshold = 450;
+int Light_Threshold = 600;
 int number = 0;
+
   
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LDR,INPUT);
   pinMode(GreenLED,OUTPUT);
 }
 
 
 void loop() {
-
 bool checker = false;
 checker = LDRTHRES();
 if(checker == true){
     digitalWrite(GreenLED,HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(GreenLED, LOW);
-    delay(500);    
+    delay(150);    
     digitalWrite(GreenLED,HIGH);
-    delay(500);
+    delay(150);
     digitalWrite(GreenLED, LOW);
-    delay(500);
     Display.show(++number);
   }
   
@@ -35,12 +42,15 @@ bool LDRTHRES(){
   bool returner = false;
   int val = analogRead(LDR);
   Serial.println(val);
-  if (val < Dark_Threshold){
+  Serial.println(val);
+  if ( state == LIGHTED && val < Dark_Threshold){
     returner = true;
+    state = SHADED;
   }
   
-  else if(val > Light_Threshold){ 
+  else if(state == SHADED && val > Light_Threshold){ 
     returner = false;
+    state = LIGHTED;
   }
 
   return returner;
